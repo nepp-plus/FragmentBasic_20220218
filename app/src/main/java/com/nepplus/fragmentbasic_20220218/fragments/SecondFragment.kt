7c9b3622c50e57cb04d399662af68d2eb2fc3c40
조경진 <cho881020@gmail.com>
2022-02-18 15:51:31 +0900

    두번째 프래그먼트 클래스 추가  - xml 끌어오기

---
+++ app/src/main/java/com/nepplus/fragmentbasic_20220218/fragments/SecondFragment.kt
@@ -0,0 +1,24 @@
+package com.nepplus.fragmentbasic_20220218.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nepplus.fragmentbasic_20220218.R
+
+class SecondFragment : Fragment() {
+
+//    1. 어떤 xml? - onCreateView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+//    2. 어떤 동작? - onActivityCreated
+
+}
